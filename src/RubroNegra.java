974f0ebff4716ef7f6a9819b96fdef938727e205
Ulpio <ulpionetto0@gmail.com>
2023-10-31 07:18:11 -0300

    Adição de remover

--- src/RubroNegra.java
+++ src/RubroNegra.java
@@ -125,6 +125,122 @@ public class RubroNegra{
         }
     }
 
+    public void remover(int valor){
+        Node z = this.raiz;
+        while(z != this.folha && z.valor != valor){
+            if(valor < z.valor){
+                z = z.esquerda;
+            }else{
+                z = z.direita;
+            }
+        }
+        if(z == this.folha){
+            System.out.println("Valor não encontrado");
+        }else{
+            Node y = z;
+            Node x;
+            String yCorOriginal = y.cor;
+            if(z.esquerda == this.folha){
+                x = z.direita;
+                this.transplantar(z, z.direita);
+            }else if(z.direita == this.folha){
+                x = z.esquerda;
+                this.transplantar(z, z.esquerda);
+            }else{
+                y = this.minimo(z.direita);
+                yCorOriginal = y.cor;
+                x = y.direita;
+                if(y.pai == z){
+                    x.pai = y;
+                }else{
+                    this.transplantar(y, y.direita);
+                    y.direita = z.direita;
+                    y.direita.pai = y;
+                }
+                this.transplantar(z, y);
+                y.esquerda = z.esquerda;
+                y.esquerda.pai = y;
+                y.cor = z.cor;
+            }
+            if(yCorOriginal == "Preto"){
+                this.removerFixup(x);
+            }
+        }
+    }
+
+    public Node minimo(Node x){
+        while(x.esquerda != this.folha){
+            x = x.esquerda;
+        }
+        return x;
+    }
+
+    public void transplantar(Node u, Node v){
+        if(u.pai == this.folha){
+            this.raiz = v;
+        }else if(u == u.pai.esquerda){
+            u.pai.esquerda = v;
+        }else{
+            u.pai.direita = v;
+        }
+        v.pai = u.pai;
+    }
+
+    public void removerFixup(Node x){
+        while(x != this.raiz && x.cor == "Preto"){
+            if(x == x.pai.esquerda){
+                Node w = x.pai.direita;
+                if(w.cor == "Vermelho"){
+                    w.cor = "Preto";
+                    x.pai.cor = "Vermelho";
+                    this.rotacaoEsquerda(x.pai);
+                    w = x.pai.direita;
+                }
+                if(w.esquerda.cor == "Preto" && w.direita.cor == "Preto"){
+                    w.cor = "Vermelho";
+                    x = x.pai;
+                }else{
+                    if(w.direita.cor == "Preto"){
+                        w.esquerda.cor = "Preto";
+                        w.cor = "Vermelho";
+                        this.rotacaoDireita(w);
+                        w = x.pai.direita;
+                    }
+                    w.cor = x.pai.cor;
+                    x.pai.cor = "Preto";
+                    w.direita.cor = "Preto";
+                    this.rotacaoEsquerda(x.pai);
+                    x = this.raiz;
+                }
+            }else{
+                Node w = x.pai.esquerda;
+                if(w.cor == "Vermelho"){
+                    w.cor = "Preto";
+                    x.pai.cor = "Vermelho";
+                    this.rotacaoDireita(x.pai);
+                    w = x.pai.esquerda;
+                }
+                if(w.direita.cor == "Preto" && w.esquerda.cor == "Preto"){
+                    w.cor = "Vermelho";
+                    x = x.pai;
+                }else{
+                    if(w.esquerda.cor == "Preto"){
+                        w.direita.cor = "Preto";
+                        w.cor = "Vermelho";
+                        this.rotacaoEsquerda(w);
+                        w = x.pai.esquerda;
+                    }
+                    w.cor = x.pai.cor;
+                    x.pai.cor = "Preto";
+                    w.esquerda.cor = "Preto";
+                    this.rotacaoDireita(x.pai);
+                    x = this.raiz;
+                }
+            }
+        }
+        x.cor = "Preto";
+    }
+
     public void listar(){
         this.listar(this.raiz);
     }
